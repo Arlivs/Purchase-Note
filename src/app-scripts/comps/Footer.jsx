0a9cf13e3238ff5ff4fd106a1f008fa7ee31a9c3
Arlivs <ilyayev00@bk.ru>
2021-09-23 16:05:23 +0300

    added footer

---
+++ src/app-scripts/comps/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+export const Footer = () => (
+<footer className="footer">
+    <div className="footer-content">asdd</div>
+    <div className="footer__rights">All rigths reserved</div>
+</footer>
+)
--- src/app-scripts/pages/Home.js
+++ src/app-scripts/pages/Home.js
@@ -1,8 +1,10 @@
 import { HomeBanner } from "../comps/HomeBanner";
 import { HomeContacts } from "../comps/HomeContacts";
+import { Footer } from "../comps/Footer";
 import '../../app-style/home.css'
 export const Home = () => (<>
     <HomeBanner/>
     <HomeContacts/>
+    <Footer/>
 </>
 )
